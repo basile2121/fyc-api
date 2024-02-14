8f91753bc88cfc88efd63590843dad625045d008
Basile2121 <basile.regnault@gmail.com>
2024-02-14 11:41:26 +0100

    Code api pour la partie VueJS

---
+++ utils/checkConnectedUser.ts
@@ -0,0 +1,16 @@
+import { Context } from "../deps.ts";
+import CookiesHandler from "./cookiesHandler.ts";
+
+export default async function getConnectedUser(ctx: Context) {
+    const userId = Number(await CookiesHandler.getCookie(ctx, 'userId'))
+
+    if (userId === undefined) {
+        ctx.response.status = 401;
+        ctx.response.body = {
+            success: false,
+            message: "Vous n\'êtes pas connecté",
+        };
+        return false;
+    }
+    return userId;
+}
